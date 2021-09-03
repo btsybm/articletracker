bfc4d43256c966458cab78836e5361e12b0550ca
btsybm <betsybeam2@gmail.com>
2021-09-02 20:02:49 -0500

    Article model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+  title = models.CharField(max_length=100)
+  link = models.CharField(max_length=200)
+  publication = models.CharField(max_length=100)
+  date = models.DateField('Date added')
+  body = models.TextField(max_length=5000)
+  notes = models.TextField(max_length=3000)
+
+  def __str__(self):
+    return self.title
